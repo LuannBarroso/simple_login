123763e197b7209531542ea38508f4e1cdb61d0a
LuannBarroso <luan23.lba@gmail.com>
2023-03-01 22:52:58 -0400

    feat: created login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,16 +8,54 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
         colors: [
           Colors.blue,
           Colors.red,
         ],
       )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: _page(),
+      ),
     );
+  }
+
+  Widget _page() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [],
+      ),
+    );
+  }
+
+  Widget _icon() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white, width: 2),
+          shape: BoxShape.circle),
+      child: const Icon(
+        Icons.person,
+        color: Colors.white,
+        size: 120,
+      ),
+    );
+  }
+
+  Widget _inputField(String hintText, TextEditingController controller) {
+    var border = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(color: Colors.white));
+
+    return const TextField();
   }
 }
